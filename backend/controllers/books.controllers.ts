64bc0ea275ae5249e5672147e50c6f444ff75c7e
Your Name <you@example.com>
2022-09-07 15:37:12 -0300

    actualizando arquitectura del proyecto

--- backend/controllers/books.controllers.ts
+++ backend/controllers/books.controllers.ts
@@ -1,67 +1,137 @@
+import {Response,Request} from 'express'
+import { Book } from '../services/Book'; 
 
-import booksModel from "../models/books.model";
+const book = new Book;
 
-export async function getAll():Promise<Object | null | undefined>{
+
+export async function getBooks(req:Request,res:Response):Promise<Response>{
     try{
-        const books = await booksModel.find(); 
-        return books;
+
+        const books = await book.getBooks();
+        return res.status(200).json(books);
+
     }catch(err){
+
         console.log(err);
+        return res.status(500).json({message:err});
     }
 }
 
-export async function getBookById(id:string):Promise <Object | null |undefined>{
-
+export async function getBooksByAuthor(req:Request,res:Response):Promise<Response>{
     try{
-        const book = await booksModel.findById(id); 
-        return book;
-    }catch(err){
-        console.log(err);
-    }
-} 
 
-export async function getBooksByName(name:string):Promise<Object | null | undefined>{
-    try{
-        const book = await booksModel.find({name:name});
-        return book;
-    }catch(err){
-        console.log(err);
-    }
-}
+        const { author } = req.params;
+        const books = await book.getBooksByAuthor(author);
 
-export async function getBooksByAuthor(author:string):Promise<Object | null | undefined>{
-    try{
-        const books = await booksModel.find({author:author}); 
-        return books;
+        if(books === 0){
+            return res.status(404).json({message:"No books found"});
+        }else{
+            return res.status(200).json(books);
+        }
+
     }catch(err){
+
         console.log(err);
+        return res.status(500).json({message:err});
     }
 }
 
+export async function getBooksByTitle(req:Request,res:Response):Promise<Response>{
+   try{
 
-export async function createBook(book:Object):Promise<Object | undefined>{
-    try{
-        const newBook = await booksModel.create(book);
-        return newBook;
+        const { title } = req.params;
+        console.log(title);
+        
+        const books = await book.getBooksByTitle(title);   
+        
+
+        if(books === 0){
+            return res.status(404).json({message:"No books found"});
+        }else{
+            return res.status(200).json(books);
+        }
+
     }catch(err){
+
         console.log(err);
+        return res.status(500).json({message:err});
     }
 }
 
-export async function editBookInfo(id:string, info:Object):Promise<Object | null | undefined>{
+export async function getBooksByAuthorAndTitle(req:Request,res:Response):Promise<Response>{
     try{
-        const book = await booksModel.findByIdAndUpdate(id, info);
-        return book;
+
+        const { author,title } = req.params;
+        const books = await book.getBooksByAuthorAndTitle(author,title);
+
+        if(books === 0){
+            return res.status(404).json({message:"No books found"});
+        }else{
+            return res.status(200).json(books);
+        }
+
     }catch(err){
+
         console.log(err);
+        return res.status(500).json({message:err});
+
     }
 }
 
-export async function deleteBook(id:string):Promise<object | undefined | null | undefined>{
-    try {
-        const book = await booksModel.findByIdAndDelete(id);
-        return book;
-    } catch (error) {
-        console.log(error);
+export async function createBook(req:Request,res:Response):Promise<Response>{
+    try{
+        const newBook = req.body; 
+        if(JSON.stringify(newBook) === "{}"){
+            return res.status(400).json({message:"Bad Request"});
+        }else{
+            const bookCreated = await book.createBook(newBook);
+            return res.status(201).json(bookCreated);
+        }
+    }catch(err){
+
+        console.log(err);
+        return res.status(500).json({message:err});
+
+    }
+}
+
+export async function editBook(req:Request,res:Response):Promise<Response>{
+    try{
+        const { id } = req.params;
+        const bookEdited = req.body;
+
+        if(JSON.stringify(bookEdited) === "{}"){
+            return res.status(400).json({message:"Bad Request"});
+        }else{
+            const bookUpdated = await book.editBook(id,bookEdited);
+            if(bookUpdated === 0){
+                return res.status(404).json({message:"Book not found"});
+            }else{
+                return res.status(200).json(bookUpdated);
+            }
+        }
+
+    }catch(err){
+            
+        console.log(err);
+        return res.status(500).json({message:err});
+    
+    }
+}
+
+export async function deleteBook(req:Request,res:Response):Promise<Response>{
+    try{
+        const { id } = req.params;
+        const bookDeleted = await book.deleteBook(id);
+        if(bookDeleted === 0){
+            return res.status(404).json({message:"Book not found"});
+        }else{
+            return res.status(200).json({message:"Book deleted"});
+        }
+    }catch(err){
+            
+            console.log(err);
+            return res.status(500).json({message:err});
+    
     }
 }
